6d89558e5cfd090863574255738af0557737c2e9
owais-ashraf <owais.ashraf72@gmail.com>
2024-08-12 15:56:31 +0500

    added vagrant file for manually and automated

---
+++ finance IAC/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostmanager",["= 1.8.10"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.32.0"]]],"checksum":"26d09810c382d91d909400b7aa3a49792f5ec891ecc63df8df7fa046470fb4bd","vagrant_version":"2.4.1"}
